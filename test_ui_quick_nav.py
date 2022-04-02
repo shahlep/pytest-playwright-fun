4f99c48c06fb067ae28de8b8c6e5f56fa2e7f854
Shah rahman <toshah.rahman@gmail.com>
2022-04-02 14:18:32 +0300

    pytest playwright fun 3

--- test_ui_quick_nav.py
+++ test_ui_quick_nav.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def test_website_navigation(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False,slow_mo=1000)
+    browser = playwright.chromium.launch(headless=True, slow_mo=1000)
     context = browser.new_context()
 
     # Open new page
