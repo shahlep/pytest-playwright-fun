dcb4d02eee10ab849150a49907fc6d22c2acebc3
Shah rahman <toshah.rahman@gmail.com>
2022-04-02 14:13:00 +0300

    pytest playwright fun 1

---
+++ test_ui_quick_nav.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def test_website_navigation(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False,slow_mo=1000)
+    context = browser.new_context()
+
+    # Open new page
+    page = context.new_page()
+
+    # Go to https://shahlep.myshopify.com/password
+    page.goto("https://shahlep.myshopify.com/password")
+
+    # Click input[name="password"]
+    page.locator("input[name=\"password\"]").click()
+    # Fill input[name="password"]
+    page.locator("input[name=\"password\"]").fill("taisia<3")
+
+    # Click button:has-text("Enter")
+    page.locator("button:has-text(\"Enter\")").click()
+    # expect(page).to_have_url("https://shahlep.myshopify.com/")
+    expect(page).to_have_url("https://shahlep.myshopify.com/")
+    #assert page.url()
+    # Click span:has-text("Catalog")
+    page.locator("span:has-text(\"Catalog\")").click()
+    # expect(page).to_have_url("https://shahlep.myshopify.com/collections/all")
+    expect(page).to_have_url("https://shahlep.myshopify.com/collections/all")
+    # Click span:has-text("Home")
+    page.locator("span:has-text(\"Home\")").click()
+    # expect(page).to_have_url("https://shahlep.myshopify.com/")
+    expect(page).to_have_url("https://shahlep.myshopify.com/")
+
+    # Click span:has-text("Contact")
+    page.locator("span:has-text(\"Contact\")").click()
+    # expect(page).to_have_url("https://shahlep.myshopify.com/pages/contact")
+    expect(page).to_have_url("https://shahlep.myshopify.com/pages/contact")
+    # Click span:has-text("shahlep")
+    page.locator("span:has-text(\"shahlep\")").click()
+    # expect(page).to_have_url("https://shahlep.myshopify.com/")
+    expect(page).to_have_url("https://shahlep.myshopify.com/")
+
+    # ---------------------
+    context.close()
+    browser.close()
