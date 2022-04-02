b4c741ae28c024dbb9e0d297da4c8b4b329f2942
Shah rahman <toshah.rahman@gmail.com>
2022-04-02 18:30:54 +0300

    pytest playwright fun 24

--- test_ui_quick_nav.py
+++ test_ui_quick_nav.py
@@ -4,6 +4,13 @@ from pytest import mark
 
 from home_page_elements import HomePage
 
+try:
+    PASSWORD = os.environ['PASSWORD']
+except KeyError:
+    import utils.secret_config
+
+    PASSWORD = utils.secret_config.PASSWORD
+
 
 @mark.ui
 def test_homepage_navigation(playwright: Playwright) -> None:
@@ -14,40 +21,38 @@ def test_homepage_navigation(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto(HomePage.home_url)
     page.pause()
-    # Click input[name="password"]
+
     page.locator(HomePage.login_password_input).click()
-    # Fill input[name="password"]
-    page.locator(HomePage.login_password_input).fill(os.environ['PASSWORD'])
-    # Click button:has-text("Enter")
+
+    page.locator(HomePage.login_password_input).fill(PASSWORD)
+
     page.locator(HomePage.login_submit).click()
-    # expect(page).to_have_url("https://shahlep.myshopify.com/")
+
     expect(page).to_have_url(HomePage.home_url)
-    # Click span:has-text("Catalog")
-    page.wait_for_selector(HomePage.navbar_catalog_btn,timeout=6000)
-    #page.locator(HomePage.navbar_catalog_btn).is_visible(timeout=5000)
+
+    #page.wait_for_selector(HomePage.navbar_catalog_btn, timeout=6000)
+
     page.locator(HomePage.navbar_catalog_btn).click()
-    # expect(page).to_have_url("https://shahlep.myshopify.com/collections/all")
+
     expect(page).to_have_url(HomePage.catalog_url)
-    # Click span:has-text("Home")
-    page.wait_for_selector(HomePage.navbar_home_btn)
-    #page.locator(HomePage.navbar_home_btn).is_visible(timeout=5000)
-    page.locator(HomePage.navbar_home_btn).click()
-    # expect(page).to_have_url("https://shahlep.myshopify.com/")
-    expect(page).to_have_url(HomePage.home_url)
 
-    # Click span:has-text("Contact")
-    page.wait_for_selector(HomePage.navbar_contact_btn)
-    #page.locator(HomePage.navbar_contact_btn).is_visible(timeout=5000)
+    #page.wait_for_selector(HomePage.navbar_home_btn)
+
+    #page.locator(HomePage.navbar_home_btn).click()
+
+    #expect(page).to_have_url(HomePage.home_url)
+
+    #page.wait_for_selector(HomePage.navbar_contact_btn)
+
     page.locator(HomePage.navbar_contact_btn).click()
-    # expect(page).to_have_url("https://shahlep.myshopify.com/pages/contact")
+
     expect(page).to_have_url(HomePage.contact_url)
-    # Click span:has-text("shahlep")
-    page.wait_for_selector(HomePage.header_btn)
-    #page.locator(HomePage.header_btn).is_visible(timeout=5000)
+
+    #page.wait_for_selector(HomePage.header_btn)
+
     page.locator(HomePage.header_btn).click()
-    # expect(page).to_have_url("https://shahlep.myshopify.com/")
+
     expect(page).to_have_url(HomePage.home_url)
 
-    # ---------------------
     context.close()
     browser.close()
